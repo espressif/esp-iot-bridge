3153705bb1ed99387d174bf5e716659d0ff1d927
Zhan Zhao Chen <zhanzhaochen@espressif.com>
2020-12-20 18:35:19 +0800

    feat: Add led & buton

--- main/app_main.c
+++ main/app_main.c
@@ -4,33 +4,41 @@
 #include "freertos/task.h"
 
 #include "esp_system.h"
+#include "esp_log.h"
 #include "esp_spi_flash.h"
+
+#include "led_gpio.h"
+
+#define LED_IO_NUM_0    25
+#define LED_IO_NUM_1    26
+
+
+static const char *TAG = "main";
 
 void app_main(void)
 {
-    printf("Hello world!\n");
+    static led_handle_t led_0, led_1;
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), WiFi%s%s, ",
-            CONFIG_IDF_TARGET,
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-
-    printf("silicon revision %d, ", chip_info.revision);
-
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+    led_0 = led_gpio_create(LED_IO_NUM_0, LED_GPIO_DARK_LOW);
+    led_1 = led_gpio_create(LED_IO_NUM_1, LED_GPIO_DARK_LOW);
+    led_gpio_state_write(led_0, LED_GPIO_ON);
+    led_gpio_state_write(led_1, LED_GPIO_OFF);
+    ESP_LOGI(TAG, "led0 state:%d", led_gpio_state_read(led_0));
+    vTaskDelay(pdMS_TO_TICKS(5000));
+    led_gpio_state_write(led_0, LED_GPIO_QUICK_BLINK);
+    led_gpio_state_write(led_1, LED_GPIO_OFF);
+    ESP_LOGI(TAG, "led0 state:%d", led_gpio_state_read(led_0));
+    ESP_LOGI(TAG, "led0 mode:%d", led_gpio_mode_read(led_0));
+    vTaskDelay(pdMS_TO_TICKS(5000));
+    led_gpio_state_write(led_0, LED_GPIO_ON);
+    led_gpio_state_write(led_1, LED_GPIO_ON);
+    vTaskDelay(pdMS_TO_TICKS(5000));
+    led_gpio_night_duty_write(20);
+    led_gpio_mode_write(led_1, LED_GPIO_NIGHT_MODE);
+    ESP_LOGI(TAG, "led0 state:%d", led_gpio_state_read(led_0));
+    ESP_LOGI(TAG, "led0 mode:%d", led_gpio_mode_read(led_0));
+    vTaskDelay(pdMS_TO_TICKS(5000));
+    led_gpio_state_write(led_1, LED_GPIO_SLOW_BLINK);
+    vTaskDelay(pdMS_TO_TICKS(5000));
+    led_gpio_mode_write(led_0, LED_GPIO_NORMAL_MODE);
 }
