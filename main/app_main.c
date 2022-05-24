7f85e880ddc0f091c2457e7c80dc55fd6579fd38
Tian Sen Wen <tiansenwen@espressif.com>
2022-05-24 11:58:21 +0800

    feat: add idf managed components

--- main/app_main.c
+++ main/app_main.c
@@ -20,16 +20,24 @@
 #include "nvs_flash.h"
 #include "esp_log.h"
 #include "esp_netif.h"
+#include "esp_timer.h"
 #include "esp_system.h"
 #include "esp_event.h"
 
 #include "esp_gateway.h"
 #include "web_server.h"
+#include "iot_button.h"
 #include "wifi_prov_mgr.h"
 
-#define GPIO_BUTTON_SW1    CONFIG_GPIO_BUTTON_SW1
+#define BUTTON_NUM            1
+#define BUTTON_SW1            CONFIG_GPIO_BUTTON_SW1
+#define BUTTON_PRESS_TIME     5000000
+#define BUTTON_REPEAT_TIME    5
 
 static const char *TAG = "main";
+static button_handle_t g_btns[BUTTON_NUM] = {0};
+static bool button_long_press = false;
+static esp_timer_handle_t restart_timer;
 
 static esp_err_t esp_storage_init(void)
 {
@@ -45,6 +53,56 @@ static esp_err_t esp_storage_init(void)
     return ret;
 }
 
+static void button_press_up_cb(void *arg)
+{
+    ESP_LOGI(TAG, "BTN: BUTTON_PRESS_UP");
+    if (button_long_press) {
+        ESP_ERROR_CHECK(esp_timer_stop(restart_timer));
+        button_long_press = false;
+    }
+}
+
+static void button_press_repeat_cb(void *arg)
+{
+    uint8_t press_repeat = iot_button_get_repeat((button_handle_t)arg);
+    ESP_LOGI(TAG, "BTN: BUTTON_PRESS_REPEAT[%d]", press_repeat);
+}
+
+static void button_long_press_start_cb(void *arg)
+{
+    ESP_LOGI(TAG, "BTN: BUTTON_LONG_PRESS_START");
+    button_long_press = true;
+    ESP_ERROR_CHECK(esp_timer_start_once(restart_timer, BUTTON_PRESS_TIME));
+}
+
+static void restart_timer_callback(void* arg)
+{
+    ESP_LOGI(TAG, "Restore factory settings");
+    nvs_flash_erase();
+    esp_restart();
+}
+
+static void esp_gateway_create_button(void)
+{
+    const esp_timer_create_args_t restart_timer_args = {
+            .callback = &restart_timer_callback,
+            .name = "restart"
+    };
+    ESP_ERROR_CHECK(esp_timer_create(&restart_timer_args, &restart_timer));
+
+    button_config_t cfg = {
+        .type = BUTTON_TYPE_GPIO,
+        .gpio_button_config = {
+            .gpio_num = BUTTON_SW1,
+            .active_level = 0,
+        },
+    };
+    g_btns[0] = iot_button_create(&cfg);
+    iot_button_register_cb(g_btns[0], BUTTON_PRESS_UP, button_press_up_cb);
+    iot_button_register_cb(g_btns[0], BUTTON_PRESS_REPEAT, button_press_repeat_cb);
+    iot_button_register_cb(g_btns[0], BUTTON_LONG_PRESS_START, button_long_press_start_cb);
+}
+
 void app_main(void)
 {
     esp_log_level_set("*", ESP_LOG_INFO);
@@ -55,6 +113,8 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     esp_gateway_create_all_netif();
+
+    esp_gateway_create_button();
 
 #if defined(CONFIG_GATEWAY_USE_WEB_SERVER)
     StartWebServer();
