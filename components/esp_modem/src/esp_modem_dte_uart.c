cc257f4bdf5c2e9c14f24a40450bde3e8816ee18
Tian Sen Wen <tiansenwen@espressif.com>
2022-10-17 16:25:26 +0800

    bugfix: change esp32-c3 modem uart source clk to UART_SCLK_XTAL

--- components/esp_modem/src/esp_modem_dte_uart.c
+++ components/esp_modem/src/esp_modem_dte_uart.c
@@ -431,10 +431,10 @@ esp_modem_dte_t *esp_modem_dte_new(const esp_modem_dte_config_t *config)
         .data_bits = config->data_bits,
         .parity = config->parity,
         .stop_bits = config->stop_bits,
-#if CONFIG_IDF_TARGET_ESP32C3 || CONFIG_IDF_TARGET_ESP32S3
-        .source_clk = UART_SCLK_RTC,
-#else
+#if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2
         .source_clk = UART_SCLK_REF_TICK,
+#else
+        .source_clk = UART_SCLK_XTAL,
 #endif
         .flow_ctrl = (config->flow_control == ESP_MODEM_FLOW_CONTROL_HW) ? UART_HW_FLOWCTRL_CTS_RTS : UART_HW_FLOWCTRL_DISABLE
     };
