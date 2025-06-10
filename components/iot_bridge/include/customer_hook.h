2221e5cd94297e55c64a55a3e05ff5606223ffd8
Xu Chun Guang <xuchunguang@espressif.com>
2025-06-10 19:55:31 +0800

    chore: Optimize some compiling workflow

--- components/iot_bridge/include/customer_hook.h
+++
@@ -1,9 +0,0 @@
-
-/*
- * SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-#pragma once
-
-int custom_hook_ip4_napt_add_failed(void);
